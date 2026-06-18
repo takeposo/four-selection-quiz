7bb6c0404cecc7259e381bd28d01eaa47fe274f4
takeposo <takeponpon@gmail.com>
2026-06-18 11:44:24 +0900

    changes the port number

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const QuizManager = require('./quizManager');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3300;
 
 // ミドルウェア
 app.use(express.static(path.join(__dirname, 'public')));
